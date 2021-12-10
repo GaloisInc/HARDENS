4155e86b406266a2839ca8f5726ca64104b2cd52
Michal Podhradsky <mpodhradsky@galois.com>
2021-12-09 17:24:37 -0800

    Verilator example added

---
+++ hardware/soc/main.cpp
@@ -0,0 +1,46 @@
+#include <verilated.h>          // Defines common routines
+#include <iostream>             // Need std::cout
+#include "Vtop.h"               // From Verilating "top.v"
+
+Vtop *top;                      // Instantiation of model
+
+vluint64_t main_time = 0;       // Current simulation time
+// This is a 64-bit integer to reduce wrap over issues and
+// allow modulus.  This is in units of the timeprecision
+// used in Verilog (or from --timescale-override)
+
+double sc_time_stamp() {        // Called by $time in Verilog
+    return main_time;           // converts to double, to match
+                                // what SystemC does
+}
+
+int main(int argc, char** argv) {
+    Verilated::commandArgs(argc, argv);   // Remember args
+
+    top = new Vtop;             // Create model
+
+    //top->reset_l = 0;           // Set some inputs
+
+    while (!Verilated::gotFinish()) {
+        // if (main_time > 10) {
+        //     top->reset_l = 1;   // Deassert reset
+        // }
+        if ((main_time % 10) == 1) {
+            top->CLK = 1;       // Toggle clock
+        }
+        if ((main_time % 10) == 6) {
+            top->CLK = 0;
+        }
+        if ((main_time % 12000000) == 0) {
+            std::cout << main_time << std::endl;       // Read a output
+            printf("LEDs: [%x,%x,%x,%x,%x,%x,%x]\n", top->LED1,top->LED2,top->LED3,top->LED4,top->LED5,top->LEDR_N,top->LEDG_N);
+        }
+        top->eval();            // Evaluate model
+        
+        main_time++;            // Time passes...
+    }
+
+    top->final();               // Done simulating
+    //    // (Though this example doesn't get here)
+    delete top;
+}
