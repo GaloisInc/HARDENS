a2360b6fdf122cd9fc71edc50e65de720289134c
Alexander Bakst <abakst@galois.com>
2021-12-27 08:50:14 -0800

    Add hand-written instrumentation system verilog

--- src/sense_actuate.c
+++ src/sense_actuate.c
@@ -6,6 +6,7 @@
 
 int instrumentation_step_generated_C(uint8_t div, struct instrumentation_state *state);
 int instrumentation_step_generated_SystemVerilog(uint8_t div, struct instrumentation_state *state);
+int instrumentation_step_handwritten_SystemVerilog(uint8_t div, struct instrumentation_state *state);
 int actuation_unit_step_generated_C(uint8_t logic_no, struct actuation_logic *state);
 int actuation_unit_step_generated_SystemVerilog(uint8_t logic_no, struct actuation_logic *state);
 
@@ -38,7 +39,7 @@ int sense_actuate_step_1(struct instrumentation_state *instrumentation,
                          struct actuation_logic *actuation)
 {
   int err = 0;
-  err |= instrumentation_step_generated_SystemVerilog(2,&instrumentation[0]);
+  err |= instrumentation_step_handwritten_SystemVerilog(2,&instrumentation[0]);
   err |= instrumentation_step_generated_SystemVerilog(3,&instrumentation[1]);
   // Do we think the devices should be actuated?
   err |= actuation_unit_step_generated_C(1,actuation);
