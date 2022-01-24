e2ebd6aa3525e6e1ffcc1083d2f45036f36f9019
Alexander Bakst <abakst@galois.com>
2022-01-24 11:21:42 -0800

    Add all-testcases driver

---
+++ tests/run_all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import subprocess
+import glob
+import sys
+import os
+
+for test in glob.glob("scripts/*"):
+    fn, ext = os.path.splitext(test)
+    if ext == "cases":
+        continue
+    print(f"{fn}:")
+    if os.path.exists(fn + ".cases"):
+        subprocess.run(["./test.py", fn, fn + ".cases"])
+    else:
+        subprocess.run(["./test.py", fn])
