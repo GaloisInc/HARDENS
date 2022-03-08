2e5a376d4b5d7ae36c83e76450eb69c2992ccca8
Michal Podhradsky <mpodhradsky@galois.com>
2022-03-08 10:30:47 -0800

    Enable ECP5 synthesis

--- hardware/SoC/firmware/firmware.c
+++ hardware/SoC/firmware/firmware.c
@@ -1,20 +1,25 @@
 #include <stdint.h>
-#include <stdio.h>
 #include "bsp.h"
+#include "printf.h"
 
-						  
+#define GPIO_REG 0x01000000
+
 int main(void)
 {
-	printf("Hello world\n");
-	while(1) {
-		printf("%d miliseconds passed\n",time_in_ms());
-		//printf("%d seconds passed...and a sensor reads 0x%X\n",time_in_s(),i2c_read(0x64, 0x0B));
+    volatile uint32_t *gpio = (void*) GPIO_REG;
+    uint32_t cnt = 0;
+    printf("Hello world\n");
+    while(1) {
+        printf("%u miliseconds passed\n",time_in_ms());
+        //printf("%d seconds passed...and a sensor reads 0x%X\n",time_in_s(),i2c_read(0x64, 0x0B));
 
-		// NOTE this is still line buffered
-		//uint8_t c = soc_getchar();
-		//printf("%c",c);
-		delay_ms(1000);
-	}
+        // NOTE this is still line buffered
+        //uint8_t c = soc_getchar();
+        //printf("%c",c);
+        *gpio = cnt;
+        cnt++;
+        delay_ms(1000);
+    }
 
-	return 0;
+    return 0;
 }
--- hardware/SoC/firmware/syscalls.c
+++ hardware/SoC/firmware/syscalls.c
@@ -2,17 +2,7 @@
 #include <sys/stat.h>
 #include <sys/times.h>
 #include <sys/time.h>
-
-void *_sbrk(int nbytes);
-int _write(int file, char *ptr, int len);
-int _close(int fd);
-int _fstat(int fd, void *buffer);
-long _lseek(int fd, long offset, int origin);
-int _read(int fd, void *buffer, unsigned int count);
-int _isatty(int fd);
-int _kill(int pid, int sig);
-int _getpid(int n);
-void _exit(int n);
+#include "syscalls.h"
 
 void _exit(int n) {
     (void)n;
