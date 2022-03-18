ebc2e23f605aa022e7c3f0777825f1f4e4fac422
Michal Podhradsky <mpodhradsky@galois.com>
2022-03-18 15:22:41 -0700

    Remove augenerated verilator files

--- src/handwritten/SystemVerilog/verilator/generate_sensor_trips/VGenerate_Sensor_Trips__ALL.d
+++
@@ -1,6 +0,0 @@
-VGenerate_Sensor_Trips__ALL.o: VGenerate_Sensor_Trips__ALL.cpp \
- VGenerate_Sensor_Trips.cpp VGenerate_Sensor_Trips.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- VGenerate_Sensor_Trips__Syms.h VGenerate_Sensor_Trips__Slow.cpp \
- VGenerate_Sensor_Trips__Syms.cpp
--- src/handwritten/SystemVerilog/verilator/is_ch_tripped/VIs_Ch_Tripped__ALL.d
+++
@@ -1,4 +0,0 @@
-VIs_Ch_Tripped__ALL.o: VIs_Ch_Tripped__ALL.cpp VIs_Ch_Tripped.cpp \
- VIs_Ch_Tripped.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h VIs_Ch_Tripped__Syms.h \
- VIs_Ch_Tripped__Slow.cpp VIs_Ch_Tripped__Syms.cpp
