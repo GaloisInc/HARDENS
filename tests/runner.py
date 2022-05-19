7f395803248eb67c8915af7a111717cff4e784dc
Michal Podhradsky <mpodhradsky@galois.com>
2022-05-19 14:21:15 -0700

    Try to tweak the test script

--- tests/runner.py
+++ tests/runner.py
@@ -65,11 +65,12 @@ def run_script(p, cmds):
                 print(f"SENDING: {c.strip()}")
             p.sendline(c.strip())
             p.sendline('')
-        time.sleep(1)
+        time.sleep(2)
     return True
 
 def run(script, args):
     p = pexpect.spawn(RTS_BIN)
+    time.sleep(2)
     with open(script) as f:
         cmds = f.readlines()
         fst = cmds[0].strip()
