90e147b9b37db807808cccedc3625bf041f18122
Michal Podhradsky <mpodhradsky@galois.com>
2022-05-19 10:55:10 -0700

    Remove unnecessary options for framac docker run

--- run_framac_proofs.sh
+++ run_framac_proofs.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -e
 
-sudo docker run --network host --privileged -v ${PWD}:/HARDENS -it \
+sudo docker run -v ${PWD}:/HARDENS -it \
     framac/frama-c:dev \
     make -C /HARDENS/src -f frama_c.mk
