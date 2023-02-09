aacfff0710b31537b5aeeca5968a4d59adcc559b
Michal Podhradsky <mpodhradsky@galois.com>
2023-02-09 15:53:21 -0800

    Update generated files (code line numbers have changed because the added license)

--- src/generated/SystemVerilog/actuator_impl.sv
+++ src/generated/SystemVerilog/actuator_impl.sv
@@ -2,6 +2,6 @@ module ActuateActuator
     ( input logic [1:0] inputs,
       output logic out
     );
-    // ../models/RTS/Actuator.cry:31:1--31:16
+    // ../models/RTS/Actuator.cry:45:1--45:16
     assign out = inputs[1] | inputs[0];
 endmodule
